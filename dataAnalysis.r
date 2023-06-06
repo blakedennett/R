ed08939e302ab51043e13bd076d1847234fae663
blakedennet2 <den18019@byui.edu>
2023-06-06 17:24:52 -0600

    getting started

---
+++ dataAnalysis.r
@@ -0,0 +1,4 @@
+
+# create dataframe
+data1 <- read_csv('C:/Users/Blake Dennett/Downloads/Spring2023/appliedProgramming/Data/stock_market_data/sp500/csv/j-p/nflx.csv')
+head(data1, 5)
---
+++ practice.r
@@ -0,0 +1,9 @@
+# printing to terminal
+ print('Hello World!')
+
+
+
+# storing variables
+ some_number <- 6
+ another_number <- 8
+ print(some_number + another_number)
