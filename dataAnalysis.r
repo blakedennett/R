d87b7d728cc5c6033d4f6c6b555b2b0331b457be
blakedennet2 <den18019@byui.edu>
2023-06-07 11:02:51 -0600

    got the dataframe up

--- dataAnalysis.r
+++ dataAnalysis.r
@@ -1,11 +1,21 @@
 
-
-
-
+# Way to use libraries/packages in R
 library(rlang)
+library(readr)
 
 
 
-# create dataframe
+# read csv file and put it into a variable
 data1 <- read_csv('C:/Users/Blake Dennett/Downloads/Spring2023/appliedProgramming/Data/stock_market_data/sp500/csv/j-p/nflx.csv')
-head(data1, 5)
+
+# turn data into a dataframe
+nflx_data <- data.frame(data1)
+
+head(nflx_data, show_col_types = False)
+
+# isolates a specific column
+nflx_data$High
+
+# first item specifies rows, second specifies column(s)
+print(nflx_data[1:5, "High"])
+
