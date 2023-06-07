56daea21b454f576b0094f8790e4d3c32d94913b
blakedennet2 <den18019@byui.edu>
2023-06-07 10:19:56 -0600

    getting rlang

--- dataAnalysis.r
+++ dataAnalysis.r
@@ -1,4 +1,11 @@
 
+
+
+
+library(rlang)
+
+
+
 # create dataframe
 data1 <- read_csv('C:/Users/Blake Dennett/Downloads/Spring2023/appliedProgramming/Data/stock_market_data/sp500/csv/j-p/nflx.csv')
 head(data1, 5)
